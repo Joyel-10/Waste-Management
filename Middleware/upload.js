b7dd93b0f18f519dd1c4fc326dd6bfd5062f6712
Joyel-10 <ashjoyel@gmail.com>
2026-01-01 12:49:13 +0530

    compalints updated

--- Middleware/upload.js
+++ Middleware/upload.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/compliants/");
+    cb(null, "uploads/complaints");
   },
   filename: (req, file, cb) => {
     cb(
